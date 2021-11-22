c376493fdeda88af0cdd9e3a25a619fb258e956f
darKleaf <2670583526@qq.com>
2021-11-22 15:27:22 +0800

    商品管理界面调整

--- src/views/commodity/CommodityManage.vue
+++ src/views/commodity/CommodityManage.vue
@@ -5,7 +5,7 @@
       :model="searchForm"
       :rules="searchRoles"
       ref="searchForm"
-      label-width="100px"
+      label-width="90px"
     >
       <el-form-item label="商品名称">
         <el-input
@@ -22,54 +22,44 @@
           <el-option label="区域二" value="beijing"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="商品名称">
-        <el-input
-          v-model="searchForm.commodityName"
-          placeholder="请输入商品名称"
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="商品名称">
-        <el-input
-          v-model="searchForm.commodityName"
-          placeholder="请输入商品名称"
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="商品名称">
-        <el-input
-          v-model="searchForm.commodityName"
-          placeholder="请输入商品名称"
-        ></el-input>
-      </el-form-item>
-      <el-form-item label="商品名称">
-        <el-input
-          v-model="searchForm.commodityName"
-          placeholder="请输入商品名称"
-        ></el-input>
+      <el-form-item label="商品状态">
+        <el-select
+          v-model="searchForm.commodityStatus"
+          placeholder="请选择商品状态"
+        >
+          <el-option label="上架" value="1"></el-option>
+          <el-option label="待上架" value="0"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item class="form-button">
         <el-button type="primary" @click="submitForm">搜索</el-button>
         <el-button @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
-    <el-table
-      ref="multipleTable"
-      :data="tableData"
-      tooltip-effect="dark"
-      style="width: 100%"
-    >
-      <el-table-column type="selection"> </el-table-column>
-      <el-table-column label="日期">
-        <template slot-scope="scope">{{ scope.row.date }}</template>
-      </el-table-column>
-      <el-table-column prop="name" label="姓名"> </el-table-column>
-      <el-table-column prop="address" label="地址" show-overflow-tooltip>
-      </el-table-column>
-    </el-table>
+    <div class="operation">
+      <el-button type="primary" icon="el-icon-upload2">上架</el-button>
+      <el-button type="primary" icon="el-icon-download">下架</el-button>
+      <el-button type="danger" icon="el-icon-edit">删除</el-button>
+    </div>
+    <div class="table">
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        border
+      >
+        <el-table-column type="selection"> </el-table-column>
+        <el-table-column label="日期">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column prop="name" label="姓名"> </el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+    </div>
     <div class="pagination">
       <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage4"
         :page-sizes="[5, 10, 15, 20]"
         :page-size="10"
         layout="total, sizes, prev, pager, next, jumper"
@@ -85,7 +75,11 @@ export default {
   name: 'CommodityManage',
   data() {
     return {
-      searchForm: {},
+      searchForm: {
+        commodityName: '',
+        commodityType: '',
+        commodityStatus: ''
+      },
       searchRoles: {},
       tableData: [
         {
@@ -138,24 +132,32 @@ export default {
   .el-form {
     background-color: #ffffff;
     padding: 20px;
+    padding-bottom: 10px;
+    padding-left: 0;
     margin-bottom: 20px;
+    overflow: hidden;
     .el-form-item {
-      div.el-form-item__content {
-        width: 300px;
-      }
+      float: left;
+      margin-bottom: 10px;
     }
     .form-button {
-      // display: block;
-      // width: 400px;
+      float: right;
       display: flex;
-      flex-direction: row-reverse;
-      div.el-form-item__content {
-        width: 400px;
-      }
     }
   }
-  .pagination {
+  .operation {
     background-color: #ffffff;
+    padding: 20px;
+    display: flex;
+    .el-button-group {
+      margin-right: 20px;
+    }
+  }
+  .table {
+    background-color: #ffffff;
+    padding-left:20px;
+  }
+  .pagination {
     display: flex;
     flex-direction: row-reverse;
   }
